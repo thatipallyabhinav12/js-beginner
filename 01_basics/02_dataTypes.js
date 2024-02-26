d666a7959046886e9d14e91f52e5374ce6e0fe21
thatipallyabhinav12 <145450421+thatipallyabhinav12@users.noreply.github.com>
2024-02-26 10:10:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node js, not browser
+
+console.log( 3 
+    +
+    3 ) // readability should be high
+
+console.log("Abhinav") // readable
+
+
+// data types
+
+// number => 12432213 (range - (2 to the power of 53) - 1) = (2 ** 53) - 1
+// bingInt
+// string
+// boolean
+// undefined
+// null => standalone value
+// symbol =>unique
+
+
+// object
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
