27ab9830bb5e1bd7acdbeb452f60886f92dde797
thatipallyabhinav12 <145450421+thatipallyabhinav12@users.noreply.github.com>
2024-02-26 09:49:11 +0000

    01_variables pushed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14355
+let accountEmail = "abhinav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+let accountState
+
+//accountId = 1212121
+
+/*
+do not use var
+it has no block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
