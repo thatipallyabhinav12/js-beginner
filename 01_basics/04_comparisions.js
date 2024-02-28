e8db68f5ba37c2555930c235ffddb420e8e378a6
Abhinav Thatipally <145450421+thatipallyabhinav12@users.noreply.github.com>
2024-02-28 16:02:10 +0000

    comparions of operators

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,15 @@
+// *********ALERT********
+// Do not encourage this type of writing a code. Write clean code.
+
+// console.log(null > 0) 
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// "==" and "comparision opeators work differently"
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// console.log("2" == 2)
+// console.log("2" === 2)
