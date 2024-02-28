8668381f80bb6ce46692867c94bb6273ddc6db02
Abhinav Thatipally <145450421+thatipallyabhinav12@users.noreply.github.com>
2024-02-28 15:25:11 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined;
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let stringToNumber = Number(score)
+
+//console.log(typeof stringToNumber)
+//console.log(stringToNumber)
+
+// "33" => 33
+// "abc" => NaN - number
+// true => 1 & false => 0
+// null => 0
+// undefined => NaN - number
+
+let isLoggedIn = "Abhinav"
+let convertedIsLoggedIn = Boolean(isLoggedIn)
+// console.log(convertedIsLoggedIn);
+
+// 0 => false; > 0 & < 0 => true
+// "" => false; "   abhinav  " => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(someNumber)
+console.log(typeof someNumber)
